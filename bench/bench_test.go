e6ff0c56c7037d4d00cbbf9f9feb7161450e4991
Karl McGuire <karl@xba.io>
2019-05-30 23:47:54 -0400

    organization

--- bench/bench_test.go
+++ bench/bench_test.go
@@ -19,8 +19,12 @@ package bench
 import (
 	"encoding/csv"
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"runtime"
+	"strconv"
+	"strings"
 	"testing"
 	"time"
 )
@@ -31,28 +35,6 @@ var PATH = flag.String("path", "stats.csv", "Filepath for benchmark CSV data.")
 
 func init() {
 	flag.Parse()
-}
-
-// save writes all logs to the PATH file in CSV format.
-func save(logs []*Log) error {
-	// will hold all log records with the first row being column labels
-	records := make([][]string, 0)
-	for _, log := range logs {
-		records = append(records, log.Record())
-	}
-	// write csv data
-	records = append([][]string{Labels()}, records...)
-	// create file for writing
-	file, err := os.OpenFile(*PATH, os.O_WRONLY|os.O_CREATE, 0666)
-	if err != nil {
-		return err
-	}
-	// clear contents
-	if err = file.Truncate(0); err != nil {
-		return err
-	}
-	defer file.Close()
-	return csv.NewWriter(file).WriteAll(records)
 }
 
 // TestMain is the entry point for running this benchmark suite.
@@ -89,5 +71,112 @@ func TestMain(m *testing.M) {
 	// save CSV to disk
 	if err := save(logs); err != nil {
 		log.Panic(err)
+	}
+}
+
+func GetSame(benchmark *Benchmark) func(b *testing.B) {
+	return func(b *testing.B) {
+		cache := benchmark.Create()
+		cache.Set("*", []byte("*"))
+		b.SetParallelism(benchmark.Para)
+		b.SetBytes(1)
+		b.ResetTimer()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				cache.Get("*")
+			}
+		})
+	}
+}
+
+// save writes all logs to the PATH file in CSV format.
+func save(logs []*Log) error {
+	// will hold all log records with the first row being column labels
+	records := make([][]string, 0)
+	for _, log := range logs {
+		records = append(records, log.Record())
+	}
+	// write csv data
+	records = append([][]string{Labels()}, records...)
+	// create file for writing
+	file, err := os.OpenFile(*PATH, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		return err
+	}
+	// clear contents
+	if err = file.Truncate(0); err != nil {
+		return err
+	}
+	defer file.Close()
+	return csv.NewWriter(file).WriteAll(records)
+}
+
+// Labels returns the column headers of the CSV data. The order is important and
+// should correspond with Log.Record().
+func Labels() []string {
+	return []string{
+		"name", "label", "goroutines", "ops", "allocs", "bytes"}
+}
+
+// Log is the primary unit of the CSV output files.
+type Log struct {
+	Benchmark *Benchmark
+	Result    *Result
+}
+
+// Record generates a CSV record.
+func (l *Log) Record() []string {
+	return []string{
+		strings.Trim(l.Benchmark.Name, " "),
+		l.Benchmark.Label,
+		fmt.Sprintf("%d", l.Benchmark.Para*l.Result.Procs),
+		fmt.Sprintf("%.2f", l.Result.Ops),
+		fmt.Sprintf("%d", l.Result.Allocs),
+		fmt.Sprintf("%d", l.Result.Bytes),
+	}
+}
+
+// Benchmark is used to generate benchmarks.
+type Benchmark struct {
+	// Name is the cache implementation identifier.
+	Name string
+	// Label is for denoting variations within implementations.
+	Label string
+	// Bencher is the function for generating testing.B benchmarks for running
+	// the actual iterations and collecting runtime information.
+	Bencher func(*Benchmark) func(b *testing.B)
+	// Para is the multiple of runtime.GOMAXPROCS(0) to use for this benchmark.
+	Para int
+	// Create is the lazily evaluated function for creating new instances of the
+	// underlying cache.
+	Create func() Cache
+}
+
+// Result is a wrapper for testing.BenchmarkResult that adds fields needed for
+// our CSV data.
+type Result struct {
+	// Ops represents millions of operations per second.
+	Ops float64
+	// Allocs is the number of allocations per iteration.
+	Allocs uint64
+	// Bytes is the number of bytes allocated per iteration.
+	Bytes uint64
+	// Procs is the value of runtime.GOMAXPROCS(0) at the time result was
+	// recorded.
+	Procs int
+}
+
+// NewResult extracts the data we're interested in from a BenchmarkResult.
+func NewResult(result testing.BenchmarkResult) *Result {
+	memops := strings.Trim(strings.Split(result.String(), "\t")[2], " MB/s")
+	opsraw, err := strconv.ParseFloat(memops, 64)
+	if err != nil {
+		log.Panic(err)
+	}
+	return &Result{
+		Ops:    opsraw,
+		Allocs: uint64(result.AllocsPerOp()),
+		Bytes:  uint64(result.AllocedBytesPerOp()),
+		Procs:  runtime.GOMAXPROCS(0),
 	}
 }
---
+++ bench/cache.go
@@ -0,0 +1,246 @@
+/*
+ * Copyright 2019 Dgraph Labs, Inc. and Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package bench
+
+import (
+	"log"
+	"sync"
+	"time"
+
+	"github.com/VictoriaMetrics/fastcache"
+	"github.com/allegro/bigcache"
+	"github.com/coocood/freecache"
+	"github.com/dgraph-io/ristretto"
+	"github.com/golang/groupcache/lru"
+)
+
+// Cache needs to be fulfilled by the cache implementations in order for the
+// benchmarks to run properly.
+type Cache interface {
+	Get(string) interface{}
+	Set(string, interface{})
+	Del(string)
+	Bench() *Stats
+}
+
+// Stats holds hit/miss information after a round of benchmark iterations has
+// been ran.
+type Stats struct {
+	Reqs uint64
+	Hits uint64
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+type BenchRistretto struct {
+	cache ristretto.Cache
+}
+
+func NewBenchRistretto(capacity int) *BenchRistretto {
+	return &BenchRistretto{
+		cache: ristretto.New(capacity),
+	}
+}
+
+func (c *BenchRistretto) Get(key string) interface{} {
+	value, _ := c.cache.Get([]byte(key))
+	return value
+}
+
+func (c *BenchRistretto) Set(key string, value interface{}) {
+	if err := c.cache.Set([]byte(key), value.([]byte)); err != nil {
+		log.Panic(err)
+	}
+}
+
+func (c *BenchRistretto) Del(key string) {
+	//c.cache.Del(key)
+}
+
+func (c *BenchRistretto) Bench() *Stats {
+	return nil
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+type BenchBaseMutex struct {
+	sync.Mutex
+	cache *lru.Cache
+}
+
+func NewBenchBaseMutex(capacity int) *BenchBaseMutex {
+	return &BenchBaseMutex{
+		cache: lru.New(capacity),
+	}
+}
+
+func (c *BenchBaseMutex) Get(key string) interface{} {
+	c.Lock()
+	defer c.Unlock()
+	value, _ := c.cache.Get(key)
+	// value found
+	return value
+}
+
+func (c *BenchBaseMutex) Set(key string, value interface{}) {
+	c.Lock()
+	defer c.Unlock()
+	c.cache.Add(key, value)
+}
+
+func (c *BenchBaseMutex) Del(key string) {
+	c.cache.Remove(key)
+}
+
+func (c *BenchBaseMutex) Bench() *Stats {
+	return nil
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+type BenchBigCache struct {
+	cache *bigcache.BigCache
+}
+
+func NewBenchBigCache(capacity int) *BenchBigCache {
+	// create a bigcache instance with default config values except for the
+	// logger - we don't want them messing with our stdout
+	//
+	// https://github.com/allegro/bigcache/blob/master/config.go#L47
+	cache, err := bigcache.NewBigCache(bigcache.Config{
+		Shards:             1024,
+		LifeWindow:         time.Second * 30,
+		CleanWindow:        0,
+		MaxEntriesInWindow: 1000 * 10 * 60,
+		MaxEntrySize:       500,
+		Verbose:            true,
+		Hasher:             newBigCacheHasher(),
+		HardMaxCacheSize:   0,
+		Logger:             nil,
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+	return &BenchBigCache{cache: cache}
+}
+
+func (c *BenchBigCache) Get(key string) interface{} {
+	value, err := c.cache.Get(key)
+	if err != nil {
+		log.Panic(err)
+	}
+	return value
+}
+
+func (c *BenchBigCache) Set(key string, value interface{}) {
+	if err := c.cache.Set(key, value.([]byte)); err != nil {
+		log.Panic(err)
+	}
+}
+
+func (c *BenchBigCache) Del(key string) {
+	if err := c.cache.Delete(key); err != nil {
+		log.Panic(err)
+	}
+}
+
+func (c *BenchBigCache) Bench() *Stats {
+	return nil
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+// bigCacheHasher is just trying to mimic bigcache's internal implementation of
+// a 64bit fnv-1a hasher
+//
+// https://github.com/allegro/bigcache/blob/master/fnv.go
+type bigCacheHasher struct{}
+
+func newBigCacheHasher() *bigCacheHasher { return &bigCacheHasher{} }
+
+func (h bigCacheHasher) Sum64(key string) uint64 {
+	hash := uint64(14695981039346656037)
+	for i := 0; i < len(key); i++ {
+		hash ^= uint64(key[i])
+		hash *= 1099511628211
+	}
+	return hash
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+type BenchFastCache struct {
+	cache *fastcache.Cache
+}
+
+func NewBenchFastCache(capacity int) *BenchFastCache {
+	return &BenchFastCache{
+		cache: fastcache.New(capacity),
+	}
+}
+
+func (c *BenchFastCache) Get(key string) interface{} {
+	var value []byte
+	c.cache.Get(value, []byte(key))
+	return value
+}
+
+func (c *BenchFastCache) Set(key string, value interface{}) {
+	c.cache.Set([]byte(key), []byte("*"))
+}
+
+func (c *BenchFastCache) Del(key string) {
+	c.cache.Del([]byte(key))
+}
+
+func (c *BenchFastCache) Bench() *Stats {
+	return nil
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+type BenchFreeCache struct {
+	cache *freecache.Cache
+}
+
+func NewBenchFreeCache(capacity int) *BenchFreeCache {
+	return &BenchFreeCache{
+		cache: freecache.NewCache(capacity),
+	}
+}
+
+func (c *BenchFreeCache) Get(key string) interface{} {
+	value, err := c.cache.Get([]byte(key))
+	if err != nil {
+		log.Panic(err)
+	}
+	return value
+}
+
+func (c *BenchFreeCache) Set(key string, value interface{}) {
+	if err := c.cache.Set([]byte(key), value.([]byte), 0); err != nil {
+		log.Panic(err)
+	}
+}
+
+func (c *BenchFreeCache) Del(key string) {
+	c.cache.Del([]byte(key))
+}
+
+func (c *BenchFreeCache) Bench() *Stats {
+	return nil
+}
