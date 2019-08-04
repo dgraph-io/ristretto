00a05f6efa11f83dc385b09346b88c25303c1864
Manish R Jain <manish@dgraph.io>
2019-08-03 20:38:30 -0700

    Arena works. Performance is in micro-seconds for allocation, which is pretty bad

--- arena.go
+++ arena.go
@@ -6,56 +6,141 @@ import (
 	"math/bits"
 	"sync"
 	"sync/atomic"
+	"unsafe"
 )
 
 type Arena struct {
-	classes    []*class
-	bigObjects map[uint64]*page
+	classes []*class
+}
+
+func NewArena() *Arena {
+	// TODO: Later support values bigger than 32KB.
+	a := new(Arena)
+	// TODO: Make the sizes more granular. TCMalloc uses 170 size classes.
+	var idx uint64
+	for size := int32(8); size <= 32*1024; size *= 2 {
+		a.classes = append(a.classes, newClass(size, idx))
+		idx++
+	}
+	return a
+}
+
+func (a *Arena) classFor(size uint32) *class {
+	if size <= 8 {
+		return a.classes[0]
+	}
+	sub := 3
+	if bits.OnesCount32(size) == 1 {
+		sub += 1
+	}
+	return a.classes[bits.Len32(size)-sub]
+}
+
+func (a *Arena) Allocate(sz uint32) ([]byte, uint64) {
+	c := a.classFor(sz)
+	if c == nil {
+		return nil, 0
+	}
+	page := c.allocatePage()
+	slot := page.allocateSlot()
+	if slot < 0 {
+		return nil, 0
+	}
+	return page.buffer(slot), uint64(slot)<<32 | uint64(page.pid)
+}
+
+func (a *Arena) Get(slot uint64) []byte {
+	pid := slot & 0xffffffff
+	cid := int(pid & 0xff)
+	if cid >= len(a.classes) {
+		return nil
+	}
+	c := a.classes[cid]
+	p, ok := c.pages.Load(pid)
+	if !ok {
+		return nil
+	}
+	page := p.(*page)
+	return page.buffer(int32(slot >> 32))
+}
+
+func Readable(slot uint64) string {
+	var buf bytes.Buffer
+	fmt.Fprintf(&buf, "Class: %d ", slot&0xff)
+	fmt.Fprintf(&buf, "Page: %d ", (slot>>8)&0xffffff)
+	fmt.Fprintf(&buf, "Slot: %d ", slot>>32)
+	return buf.String()
 }
 
 type class struct {
-	sync.RWMutex
-	nextId     uint32
+	mask       uint64 // Uses 8-bits in LSB.
+	nextId     *uint32
 	objectSize int32
-	pages      map[uint32]*page
+	pages      *sync.Map
+	latest     *unsafe.Pointer
+}
+
+func newClass(size int32, mask uint64) *class {
+	return &class{
+		mask:       mask,
+		nextId:     new(uint32),
+		objectSize: size,
+		pages:      new(sync.Map),
+		latest:     new(unsafe.Pointer),
+	}
 }
 
 func (c *class) availablePage() *page {
-	c.RLock()
-	defer c.RUnlock()
-	for _, p := range c.pages {
-		if p.availableSlots() > 0 {
-			return p
+	if cached := (*page)(atomic.LoadPointer(c.latest)); cached != nil {
+		if cached.availableSlots() > 0 {
+			return cached
 		}
 	}
-	return nil
+	var out *page
+	c.pages.Range(func(key, value interface{}) bool {
+		p := value.(*page)
+		if p.availableSlots() > 0 {
+			out = p
+			return false
+		}
+		return true
+	})
+	if out != nil {
+		atomic.StorePointer(c.latest, unsafe.Pointer(out))
+	}
+	return out
 }
 
+const maxNextId = uint32(1<<24 - 1)
+
 func (c *class) allocatePage() *page {
-	if p := c.availablePage(); c != nil {
+	if p := c.availablePage(); p != nil {
 		return p
 	}
 	// Need to figure out locking situation here.
-	c.Lock()
-	p := newPage(c.nextId, c.objectSize)
-	c.pages[p.pid] = p
-	c.nextId++
-	c.Unlock()
-	return p
+	nextId := atomic.AddUint32(c.nextId, 1)
+	if nextId >= maxNextId {
+		return nil // We can only use 24-bits.
+	}
+	pid := c.mask | uint64(nextId<<8)
+	out := newPage(pid, c.objectSize)
+	c.pages.Store(out.pid, out)
+	return out
 }
 
 type page struct {
-	pid       uint32
+	pid       uint64
 	size      int32
 	data      []byte
 	bits      []*uint32
-	allocated *int32
-	released  *int32
+	allocated *int32 // These two can help figure out when to release this page.
+	released  *int32 // Not being used right now.
+	leftSlots *int32
 }
 
 const defaultPageSize int32 = 4096
 
-func newPage(pid uint32, size int32) *page {
+func newPage(pid uint64, size int32) *page {
 	slots := defaultPageSize / size
 	if slots < 32 {
 		slots = 32
@@ -74,15 +159,16 @@ func newPage(pid uint32, size int32) *page {
 		bits:      b,
 		allocated: new(int32),
 		released:  new(int32),
+		leftSlots: &slots,
 	}
 }
 
 func (p *page) availableSlots() int32 {
-	return int32(len(p.bits)*32) - (atomic.LoadInt32(p.allocated) - atomic.LoadInt32(p.released))
+	return atomic.LoadInt32(p.leftSlots)
 }
 
 // Does not need any more locks beyond atomics.
-func (p *page) allocateSlot() int {
+func (p *page) allocateSlot() int32 {
 	for idx := 0; idx < len(p.bits); {
 		bit := atomic.LoadUint32(p.bits[idx])
 		tmp := ^bit
@@ -98,14 +184,14 @@ func (p *page) allocateSlot() int {
 		if !atomic.CompareAndSwapUint32(p.bits[idx], bit, newBit) {
 			continue // Try again.
 		}
-		atomic.AddInt32(p.allocated, 1)
-		return 32*idx + slot
+		atomic.AddInt32(p.leftSlots, -1)
+		return int32(32*idx + slot)
 	}
 	return -1
 }
 
 // Does not need any more locks beyond atomics.
-func (p *page) releaseSlot(slot int) {
+func (p *page) releaseSlot(slot int32) {
 	if slot < 0 {
 		return
 	}
@@ -114,7 +200,7 @@ func (p *page) releaseSlot(slot int) {
 	for {
 		old := atomic.LoadUint32(p.bits[idx])
 		if atomic.CompareAndSwapUint32(p.bits[idx], old, old & ^mask) {
-			atomic.AddInt32(p.released, 1)
+			atomic.AddInt32(p.leftSlots, 1)
 			return
 		}
 	}
@@ -132,16 +218,4 @@ func (p *page) String() string {
 		fmt.Fprintf(&buf, "idx=%d slots=%0b ", idx, atomic.LoadUint32(bit))
 	}
 	return buf.String()
-}
-
-const maxSize int = 32 << 10
-
-func (a *Arena) Allocate(data []byte) uint64 {
-	if len(data) > maxSize {
-		// allocate on big objects.
-	}
-	// pow := math.Ceil((math.Log2(float64(len(data)))))
-	// c := a.classes[int(pow)]
-
-	return 0
 }
--- arena_test.go
+++ arena_test.go
@@ -1,6 +1,13 @@
 package ristretto
 
-import "testing"
+import (
+	"math/rand"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestPage(t *testing.T) {
 	p := newPage(1, 32)
@@ -8,8 +15,65 @@ func TestPage(t *testing.T) {
 		t.Logf("Slot: %v\n", p.allocateSlot())
 	}
 	t.Logf("Final: %s\n", p)
-	for i := 0; i < 32*4; i++ {
+	for i := int32(0); i < 32*4; i++ {
 		p.releaseSlot(i)
 		t.Logf("Final: %s\n", p)
 	}
+}
+
+func TestArena(t *testing.T) {
+	a := NewArena()
+	require.Equal(t, int32(8), a.classFor(7).objectSize)
+	require.Equal(t, int32(16), a.classFor(9).objectSize)
+	require.Equal(t, int32(16), a.classFor(16).objectSize)
+	require.Equal(t, int32(32), a.classFor(17).objectSize)
+	require.Equal(t, int32(32), a.classFor(31).objectSize)
+	require.Equal(t, int32(32), a.classFor(32).objectSize)
+	require.Equal(t, int32(1024), a.classFor(513).objectSize)
+	require.Equal(t, int32(32*1024), a.classFor(16*1024+1).objectSize)
+}
+
+func TestAllocation(t *testing.T) {
+	a := NewArena()
+	buf, slot := a.Allocate(13)
+	require.NotEqual(t, 0, slot)
+	rand.Read(buf)
+	require.Equal(t, buf, a.Get(slot))
+
+	slots := make(map[uint64]struct{})
+	for i := 0; i < 100; i++ {
+		buf, slot = a.Allocate(452)
+		if slot == 0 {
+			pg := a.classFor(452).availablePage()
+			t.Logf("avail: %d", pg.availableSlots())
+		}
+		require.NotEqual(t, 0, slot)
+		require.Equal(t, 512, len(buf))
+		if _, has := slots[slot]; has {
+			require.False(t, has)
+		}
+		slots[slot] = struct{}{}
+		t.Logf("%s", Readable(slot))
+	}
+}
+
+func BenchmarkAlloc(b *testing.B) {
+	a := NewArena()
+	var success, fail uint64
+	b.RunParallel(func(pb *testing.PB) {
+		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		var ok, not uint64
+		for pb.Next() {
+			sz := uint32(r.Intn(512))
+			_, slot := a.Allocate(sz)
+			if slot == 0 {
+				not++
+			} else {
+				ok++
+			}
+		}
+		atomic.AddUint64(&fail, not)
+		atomic.AddUint64(&success, ok)
+	})
+	b.Logf("OK: %d. Fail: %d", success, fail)
 }
