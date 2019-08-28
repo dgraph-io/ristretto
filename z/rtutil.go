79c537e7e818fdc789c364d7cf2534a15815dc77
Ashish Goswami <ashish@dgraph.io>
2019-08-28 21:22:16 +0530

    Minor fix

--- z/rtutil.go
+++ z/rtutil.go
@@ -43,15 +43,15 @@ type stringStruct struct {
 //go:linkname memhash runtime.memhash
 func memhash(p unsafe.Pointer, h, s uintptr) uintptr
 
-// MemHash is the hash function used by map, it utilizes available hardware instructions(behaves as
-// aeshash if aes instruction is available).
+// MemHash is the hash function used by go map, it utilizes available hardware instructions(behaves
+// as aeshash if aes instruction is available).
 // NOTE: The hash seed changes for every process. So, this cannot be used as a persistent hash.
 func MemHash(data []byte) uint64 {
 	ss := (*stringStruct)(unsafe.Pointer(&data))
 	return uint64(memhash(ss.str, 0, uintptr(ss.len)))
 }
 
-// MemHashString is the hash function used by map, it utilizes available hardware instructions
+// MemHashString is the hash function used by go map, it utilizes available hardware instructions
 // (behaves as aeshash if aes instruction is available).
 // NOTE: The hash seed changes for every process. So, this cannot be used as a persistent hash.
 func MemHashString(str string) uint64 {
