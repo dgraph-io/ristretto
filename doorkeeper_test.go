eb48d2f7ca30f7560340ff99eccf24c756d42c2b
Ashish Goswami <ashish@dgraph.io>
2019-09-03 12:13:22 +0530

    Replace AESHash with MemHash (#37)
    Currently we are using AESHash(aeshash in runtime) which uses
    aesenc instruction. This can crash restritto if it is running
    on a hardware not having this instruction. Go runtime, at init
    decides to use memhash if this instruction is not available.
    memhash is independent of hardware. Also memhash on every call,
    checks if aeshash can be calculated(based on the availabilty of
    aesenc intruction). This commit replaces AESHash with MemHash.

--- doorkeeper_test.go
+++ doorkeeper_test.go
@@ -24,7 +24,7 @@ import (
 
 func TestDoorkeeper(t *testing.T) {
 	d := z.NewBloomFilter(float64(1374), 0.01)
-	hash := z.AESHashString("*")
+	hash := z.MemHashString("*")
 	if d.Has(hash) {
 		t.Fatal("item exists but was never added")
 	}
