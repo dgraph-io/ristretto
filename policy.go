d963fa241740c4012b003362a7602e6ae764b104
Karl McGuire <karl@karlmcguire.com>
2019-10-01 10:22:46 -0400

    immediate Updates and lazy cost evaluation (#75)
    * coster function and testing
    
    * coster documentation
    
    * cacheUpdate test fix
    
    * cost and set/del fixes
    
    * remove defers

--- policy.go
+++ policy.go
@@ -43,6 +43,10 @@ type policy interface {
 	Del(uint64)
 	// Cap returns the available capacity.
 	Cap() int64
+	// Update updates the cost value for the key.
+	Update(uint64, int64)
+	// Cost returns the cost value of a key or -1 if missing.
+	Cost(uint64) int64
 	// Optionally, set stats object to track how policy is performing.
 	CollectMetrics(stats *metrics)
 }
@@ -157,9 +161,7 @@ func (p *defaultPolicy) Add(key uint64, cost int64) ([]*item, bool) {
 		// store victim in evicted victims slice
 		victims = append(victims, &item{
 			key:  minKey,
-			val:  nil,
 			cost: minCost,
-			del:  false,
 		})
 	}
 	p.evict.add(key, cost)
@@ -168,21 +170,37 @@ func (p *defaultPolicy) Add(key uint64, cost int64) ([]*item, bool) {
 
 func (p *defaultPolicy) Has(key uint64) bool {
 	p.Lock()
-	defer p.Unlock()
 	_, exists := p.evict.keyCosts[key]
+	p.Unlock()
 	return exists
 }
 
 func (p *defaultPolicy) Del(key uint64) {
 	p.Lock()
-	defer p.Unlock()
 	p.evict.del(key)
+	p.Unlock()
 }
 
 func (p *defaultPolicy) Cap() int64 {
 	p.Lock()
+	capacity := int64(p.evict.maxCost - p.evict.used)
+	p.Unlock()
+	return capacity
+}
+
+func (p *defaultPolicy) Update(key uint64, cost int64) {
+	p.Lock()
+	p.evict.updateIfHas(key, cost)
+	p.Unlock()
+}
+
+func (p *defaultPolicy) Cost(key uint64) int64 {
+	p.Lock()
 	defer p.Unlock()
-	return int64(p.evict.maxCost - p.evict.used)
+	if cost, found := p.evict.keyCosts[key]; found {
+		return cost
+	}
+	return -1
 }
 
 // sampledLFU is an eviction helper storing key-cost pairs.
@@ -222,10 +240,8 @@ func (p *sampledLFU) del(key uint64) {
 	if !ok {
 		return
 	}
-
 	p.stats.Add(keyEvict, key, 1)
 	p.stats.Add(costEvict, key, uint64(cost))
-
 	p.used -= cost
 	delete(p.keyCosts, key)
 }
@@ -233,16 +249,14 @@ func (p *sampledLFU) del(key uint64) {
 func (p *sampledLFU) add(key uint64, cost int64) {
 	p.stats.Add(keyAdd, key, 1)
 	p.stats.Add(costAdd, key, uint64(cost))
-
 	p.keyCosts[key] = cost
 	p.used += cost
 }
 
-// TODO: Move this to the store itself. So, it can be used by public Set.
-func (p *sampledLFU) updateIfHas(key uint64, cost int64) (updated bool) {
-	if prev, exists := p.keyCosts[key]; exists {
-		// Update the cost of the existing key. For simplicity, don't worry about evicting anything
-		// if the updated cost causes the size to grow beyond maxCost.
+func (p *sampledLFU) updateIfHas(key uint64, cost int64) bool {
+	if prev, found := p.keyCosts[key]; found {
+		// update the cost of an existing key, but don't worry about evicting,
+		// evictions will be handled the next time a new item is added
 		p.stats.Add(keyUpdate, key, 1)
 		p.used += cost - prev
 		p.keyCosts[key] = cost
@@ -376,9 +390,7 @@ func (p *lruPolicy) Add(key uint64, cost int64) ([]*item, bool) {
 		delete(p.ptrs, victim.key)
 		victims = append(victims, &item{
 			key:  victim.key,
-			val:  nil,
 			cost: victim.cost,
-			del:  false,
 		})
 		// adjust room
 		p.room += victim.cost
@@ -410,6 +422,15 @@ func (p *lruPolicy) Cap() int64 {
 	p.Lock()
 	defer p.Unlock()
 	return int64(p.vals.Len())
+}
+
+// TODO
+func (p *lruPolicy) Update(key uint64, cost int64) {
+}
+
+// TODO
+func (p *lruPolicy) Cost(key uint64) int64 {
+	return -1
 }
 
 // TODO
