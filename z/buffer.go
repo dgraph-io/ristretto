148048a248734999ed5a85d434f22c797ae93383
Manish R Jain <manish@dgraph.io>
2020-09-22 07:50:18 -0700

    Introduce Mmapped buffers and Merge Sort (#194)
    Buffers can now be mmapped as well as Calloc'd. This PR also copies over all the mmap files from Badger to allow mmap support in various platforms.
    
    This PR also introduces Merge Sort to do sorting of the buffer using an extra temporary space costing half of the space as the original buffer, currently allocated on Calloc. We can't use quick sort. Each entry is variable length, so we can't just swap them in the buffer. Merge Sort allows us to iterate over them linearly, hence is a better fit.

--- z/buffer.go
+++ z/buffer.go
@@ -18,21 +18,114 @@ package z
 
 import (
 	"encoding/binary"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math"
+	"os"
+	"sort"
+
+	"github.com/pkg/errors"
 )
 
-// Buffer is equivalent of bytes.Buffer without the ability to read. It uses z.Calloc to allocate
-// memory, which depending upon how the code is compiled could use jemalloc for allocations.
+// Buffer is equivalent of bytes.Buffer without the ability to read. It is NOT thread-safe.
+//
+// In UseCalloc mode, z.Calloc is used to allocate memory, which depending upon how the code is
+// compiled could use jemalloc for allocations.
+//
+// In UseMmap mode, Buffer  uses file mmap to allocate memory. This allows us to store big data
+// structures without using physical memory.
+//
+// MaxSize can be set to limit the memory usage.
 type Buffer struct {
-	buf    []byte
-	offset int
+	buf     []byte
+	offset  int
+	curSz   int
+	maxSz   int
+	fd      *os.File
+	bufType BufferType
 }
 
-// NewBuffer would allocate a buffer of size sz upfront.
-func NewBuffer(sz int) *Buffer {
-	return &Buffer{
-		buf:    Calloc(sz),
-		offset: 0,
+type BufferType int
+
+func (t BufferType) String() string {
+	switch t {
+	case UseCalloc:
+		return "UseCalloc"
+	case UseMmap:
+		return "UseMmap"
 	}
+	return "invalid"
+}
+
+const (
+	UseCalloc BufferType = iota
+	UseMmap
+	UseInvalid
+)
+
+// smallBufferSize is an initial allocation minimal capacity.
+const smallBufferSize = 64
+
+// Newbuffer is a helper utility, which creates a virtually unlimited Buffer in UseCalloc mode.
+func NewBuffer(sz int) *Buffer {
+	buf, err := NewBufferWith(sz, math.MaxInt64, UseCalloc)
+	if err != nil {
+		log.Fatalf("while creating buffer: %v", err)
+	}
+	return buf
+}
+
+// NewBufferWith would allocate a buffer of size sz upfront, with the total size of the buffer not
+// exceeding maxSz. Both sz and maxSz can be set to zero, in which case reasonable defaults would be
+// used. Buffer can't be used without initialization via NewBuffer.
+func NewBufferWith(sz, maxSz int, bufType BufferType) (*Buffer, error) {
+	var buf []byte
+	var fd *os.File
+
+	if sz == 0 {
+		sz = smallBufferSize
+	}
+	if maxSz == 0 {
+		maxSz = math.MaxInt32
+	}
+
+	switch bufType {
+	case UseCalloc:
+		buf = Calloc(sz)
+
+	case UseMmap:
+		var err error
+		fd, err = ioutil.TempFile("", "buffer")
+		if err != nil {
+			return nil, err
+		}
+		if err := fd.Truncate(int64(sz)); err != nil {
+			return nil, errors.Wrapf(err, "while truncating %s to size: %d", fd.Name(), sz)
+		}
+
+		buf, err = Mmap(fd, true, int64(maxSz)) // Mmap up to max size.
+		if err != nil {
+			return nil, errors.Wrapf(err, "while mmapping %s with size: %d", fd.Name(), maxSz)
+		}
+
+	default:
+		log.Fatalf("Invalid bufType: %q\n", bufType)
+	}
+
+	buf[0] = 0x00
+	return &Buffer{
+		buf:     buf,
+		offset:  1, // Always leave offset 0.
+		curSz:   sz,
+		maxSz:   maxSz,
+		fd:      fd,
+		bufType: bufType,
+	}, nil
+}
+
+func (b *Buffer) IsEmpty() bool {
+	return b.offset == 1
 }
 
 // Len would return the number of bytes written to the buffer so far.
@@ -42,33 +135,48 @@ func (b *Buffer) Len() int {
 
 // Bytes would return all the written bytes as a slice.
 func (b *Buffer) Bytes() []byte {
-	return b.buf[0:b.offset]
+	return b.buf[1:b.offset]
 }
-
-// smallBufferSize is an initial allocation minimal capacity.
-const smallBufferSize = 64
 
 // Grow would grow the buffer to have at least n more bytes. In case the buffer is at capacity, it
 // would reallocate twice the size of current capacity + n, to ensure n bytes can be written to the
-// buffer without further allocation.
+// buffer without further allocation. In UseMmap mode, this might result in underlying file
+// expansion.
 func (b *Buffer) Grow(n int) {
 	// In this case, len and cap are the same.
-	if len(b.buf) == 0 && n <= smallBufferSize {
-		b.buf = Calloc(smallBufferSize)
-		return
-	} else if b.buf == nil {
-		b.buf = Calloc(n)
-		return
+	if b.buf == nil {
+		panic("z.Buffer needs to be initialized before using")
 	}
-	if b.offset+n < len(b.buf) {
+	if b.maxSz-b.offset < n {
+		panic(fmt.Sprintf("Buffer max size exceeded: %d."+
+			" Offset: %d. Grow: %d", b.maxSz, b.offset, n))
+	}
+	if b.curSz-b.offset > n {
 		return
 	}
 
-	sz := 2*len(b.buf) + n
-	newBuf := Calloc(sz)
-	copy(newBuf, b.buf[:b.offset])
-	Free(b.buf)
-	b.buf = newBuf
+	growBy := b.curSz + n
+	if growBy > 1<<30 {
+		growBy = 1 << 30
+	}
+	if n > growBy {
+		// Always at least allocate n, even if it exceeds the 1GB limit above.
+		growBy = n
+	}
+	b.curSz += growBy
+
+	switch b.bufType {
+	case UseCalloc:
+		newBuf := Calloc(b.curSz)
+		copy(newBuf, b.buf[:b.offset])
+		Free(b.buf)
+		b.buf = newBuf
+	case UseMmap:
+		if err := b.fd.Truncate(int64(b.curSz)); err != nil {
+			log.Fatalf("While trying to truncate file %s to size: %d error: %v\n",
+				b.fd.Name(), b.curSz, err)
+		}
+	}
 }
 
 // Allocate is a way to get a slice of size n back from the buffer. This slice can be directly
@@ -81,6 +189,14 @@ func (b *Buffer) Allocate(n int) []byte {
 	return b.buf[off:b.offset]
 }
 
+// AllocateOffset works the same way as allocate, but instead of returning a byte slice, it returns
+// the offset of the allocation.
+func (b *Buffer) AllocateOffset(n int) int {
+	b.Grow(n)
+	b.offset += n
+	return b.offset - n
+}
+
 func (b *Buffer) writeLen(sz int) {
 	buf := b.Allocate(4)
 	binary.BigEndian.PutUint32(buf, uint32(sz))
@@ -89,31 +205,188 @@ func (b *Buffer) writeLen(sz int) {
 // SliceAllocate would encode the size provided into the buffer, followed by a call to Allocate,
 // hence returning the slice of size sz. This can be used to allocate a lot of small buffers into
 // this big buffer.
-// Note that SliceAllocate should NOT be mixed with normal calls to Write. Otherwise, SliceOffsets
-// won't work.
+// Note that SliceAllocate should NOT be mixed with normal calls to Write.
 func (b *Buffer) SliceAllocate(sz int) []byte {
 	b.Grow(4 + sz)
 	b.writeLen(sz)
 	return b.Allocate(sz)
 }
 
-// SliceOffsets would return the offsets of all slices written to the buffer.
-// TODO: Perhaps keep the offsets separate in another buffer, and allow access to slices via index.
-func (b *Buffer) SliceOffsets(offsets []int) []int {
-	start := 0
-	for start < b.offset {
-		offsets = append(offsets, start)
-		sz := binary.BigEndian.Uint32(b.buf[start:])
-		start += 4 + int(sz)
+type LessFunc func(a, b []byte) bool
+type sortHelper struct {
+	offsets []int
+	b       *Buffer
+	tmp     *Buffer
+	less    LessFunc
+	small   []int
+}
+
+func (s *sortHelper) sortSmall(start, end int) {
+	s.tmp.Reset()
+	s.small = s.small[:0]
+	next := start
+	for next != 0 && next < end {
+		s.small = append(s.small, next)
+		_, next = s.b.Slice(next)
 	}
-	return offsets
+
+	// We are sorting the slices pointed to by s.small offsets, but only moving the offsets around.
+	sort.Slice(s.small, func(i, j int) bool {
+		left, _ := s.b.Slice(s.small[i])
+		right, _ := s.b.Slice(s.small[j])
+		return s.less(left, right)
+	})
+	// Now we iterate over the s.small offsets and copy over the slices. The result is now in order.
+	for _, off := range s.small {
+		s.tmp.Write(rawSlice(s.b.buf[off:]))
+	}
+	assert(end-start == copy(s.b.buf[start:end], s.tmp.Bytes()))
+}
+
+func assert(b bool) {
+	if !b {
+		log.Fatalf("Assertion failure")
+	}
+}
+func check(err error) {
+	assert(err == nil)
+}
+func check2(_ interface{}, err error) {
+	check(err)
+}
+
+func (s *sortHelper) merge(left, right []byte, start, end int) {
+	if len(left) == 0 || len(right) == 0 {
+		return
+	}
+	s.tmp.Reset()
+	check2(s.tmp.Write(left))
+	left = s.tmp.Bytes()
+
+	var ls, rs []byte
+
+	copyLeft := func() {
+		assert(len(ls) == copy(s.b.buf[start:], ls))
+		left = left[len(ls):]
+		start += len(ls)
+	}
+	copyRight := func() {
+		assert(len(rs) == copy(s.b.buf[start:], rs))
+		right = right[len(rs):]
+		start += len(rs)
+	}
+
+	for start < end {
+		if len(left) == 0 {
+			assert(len(right) == copy(s.b.buf[start:end], right))
+			return
+		}
+		if len(right) == 0 {
+			assert(len(left) == copy(s.b.buf[start:end], left))
+			return
+		}
+		ls = rawSlice(left)
+		rs = rawSlice(right)
+
+		// We skip the first 4 bytes in the rawSlice, because that stores the length.
+		if s.less(ls[4:], rs[4:]) {
+			copyLeft()
+		} else {
+			copyRight()
+		}
+	}
+}
+
+func (s *sortHelper) sort(lo, hi int) []byte {
+	assert(lo <= hi)
+
+	mid := lo + (hi-lo)/2
+	loff, hoff := s.offsets[lo], s.offsets[hi]
+	if lo == mid {
+		// No need to sort, just return the buffer.
+		return s.b.buf[loff:hoff]
+	}
+
+	// lo, mid would sort from [offset[lo], offset[mid]) .
+	left := s.sort(lo, mid)
+	// Typically we'd use mid+1, but here mid represents an offset in the buffer. Each offset
+	// contains a thousand entries. So, if we do mid+1, we'd skip over those entries.
+	right := s.sort(mid, hi)
+
+	s.merge(left, right, loff, hoff)
+	return s.b.buf[loff:hoff]
+}
+
+// SortSlice is like SortSliceBetween but sorting over the entire buffer.
+func (b *Buffer) SortSlice(less func(left, right []byte) bool) {
+	b.SortSliceBetween(1, b.offset, less)
+}
+func (b *Buffer) SortSliceBetween(start, end int, less LessFunc) {
+	if start >= end {
+		return
+	}
+	if start == 0 {
+		panic("start can never be zero")
+	}
+
+	var offsets []int
+	next, count := start, 0
+	for next != 0 && next < end {
+		if count%1024 == 0 {
+			offsets = append(offsets, next)
+		}
+		_, next = b.Slice(next)
+		count++
+	}
+	assert(len(offsets) > 0)
+	if offsets[len(offsets)-1] != end {
+		offsets = append(offsets, end)
+	}
+
+	szTmp := int(float64((end-start)/2) * 1.1)
+	s := &sortHelper{
+		offsets: offsets,
+		b:       b,
+		less:    less,
+		small:   make([]int, 0, 1024),
+		tmp:     NewBuffer(szTmp),
+	}
+	defer s.tmp.Release()
+
+	left := offsets[0]
+	for _, off := range offsets[1:] {
+		s.sortSmall(left, off)
+		left = off
+	}
+	s.sort(0, len(offsets)-1)
+}
+
+func rawSlice(buf []byte) []byte {
+	sz := binary.BigEndian.Uint32(buf)
+	return buf[:4+int(sz)]
 }
 
 // Slice would return the slice written at offset.
-func (b *Buffer) Slice(offset int) []byte {
+func (b *Buffer) Slice(offset int) ([]byte, int) {
+	if offset >= b.offset {
+		return nil, 0
+	}
+
 	sz := binary.BigEndian.Uint32(b.buf[offset:])
 	start := offset + 4
-	return b.buf[start : start+int(sz)]
+	next := start + int(sz)
+	res := b.buf[start:next]
+	if next >= b.offset {
+		next = 0
+	}
+	return res, next
+}
+
+func (b *Buffer) Data(offset int) []byte {
+	if offset > b.curSz {
+		panic("offset beyond current size")
+	}
+	return b.buf[offset:b.curSz]
 }
 
 // Write would write p bytes to the buffer.
@@ -126,11 +399,30 @@ func (b *Buffer) Write(p []byte) (n int, err error) {
 
 // Reset would reset the buffer to be reused.
 func (b *Buffer) Reset() {
-	b.offset = 0
+	b.offset = 1
 }
 
 // Release would free up the memory allocated by the buffer. Once the usage of buffer is done, it is
 // important to call Release, otherwise a memory leak can happen.
-func (b *Buffer) Release() {
-	Free(b.buf)
+func (b *Buffer) Release() error {
+	switch b.bufType {
+	case UseCalloc:
+		Free(b.buf)
+
+	case UseMmap:
+		fname := b.fd.Name()
+		if err := Munmap(b.buf); err != nil {
+			return errors.Wrapf(err, "while munmap file %s", fname)
+		}
+		if err := b.fd.Truncate(0); err != nil {
+			return errors.Wrapf(err, "while truncating file %s", fname)
+		}
+		if err := b.fd.Close(); err != nil {
+			return errors.Wrapf(err, "while closing file %s", fname)
+		}
+		if err := os.Remove(b.fd.Name()); err != nil {
+			return errors.Wrapf(err, "while deleting file %s", fname)
+		}
+	}
+	return nil
 }
--- z/buffer_test.go
+++ z/buffer_test.go
@@ -18,85 +18,205 @@ package z
 
 import (
 	"bytes"
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
 	"math/rand"
+	"sort"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestCallocBuffer(t *testing.T) {
+func TestBuffer(t *testing.T) {
 	rand.Seed(time.Now().Unix())
 
-	var bytesBuffer bytes.Buffer // This is just for verifying result.
-	bytesBuffer.Grow(512)
+	for btype := UseCalloc; btype < UseInvalid; btype++ {
+		name := fmt.Sprintf("Using mode %s", btype)
+		t.Run(name, func(t *testing.T) {
+			var bytesBuffer bytes.Buffer // This is just for verifying result.
+			bytesBuffer.Grow(512)
 
-	cBuffer := NewBuffer(512)
+			cBuffer, err := NewBufferWith(512, 4<<30, btype)
+			require.Nil(t, err)
+			defer cBuffer.Release()
 
-	// Writer small []byte
-	var smallBytes [256]byte
-	rand.Read(smallBytes[:])
-	var bigBytes [1024]byte
-	rand.Read(bigBytes[:])
+			// Writer small []byte
+			var smallBytes [256]byte
+			rand.Read(smallBytes[:])
+			var bigBytes [1024]byte
+			rand.Read(bigBytes[:])
 
-	_, err := cBuffer.Write(smallBytes[:])
-	require.NoError(t, err, "unable to write data to page buffer")
-	_, err = cBuffer.Write(bigBytes[:])
-	require.NoError(t, err, "unable to write data to page buffer")
+			_, err = cBuffer.Write(smallBytes[:])
+			require.NoError(t, err, "unable to write data to page buffer")
+			_, err = cBuffer.Write(bigBytes[:])
+			require.NoError(t, err, "unable to write data to page buffer")
 
-	// Write data to bytesBuffer also, just to match result.
-	bytesBuffer.Write(smallBytes[:])
-	bytesBuffer.Write(bigBytes[:])
+			// Write data to bytesBuffer also, just to match result.
+			bytesBuffer.Write(smallBytes[:])
+			bytesBuffer.Write(bigBytes[:])
 
-	require.True(t, bytes.Equal(cBuffer.Bytes(), bytesBuffer.Bytes()))
-}
-
-func TestCallocBufferWrite(t *testing.T) {
-	rand.Seed(time.Now().Unix())
-
-	var wb [128]byte
-	rand.Read(wb[:])
-
-	cb := NewBuffer(32)
-	bb := new(bytes.Buffer)
-
-	end := 32
-	for i := 0; i < 3; i++ {
-		n, err := cb.Write(wb[:end])
-		require.NoError(t, err, "unable to write bytes to buffer")
-		require.Equal(t, n, end, "length of buffer and length written should be equal")
-
-		// append to bb also for testing.
-		bb.Write(wb[:end])
-
-		require.True(t, bytes.Equal(cb.Bytes(), bb.Bytes()), "Both bytes should match")
-		end = end * 2
+			require.True(t, bytes.Equal(cBuffer.Bytes(), bytesBuffer.Bytes()))
+		})
 	}
 }
 
-func TestSliceAlloc(t *testing.T) {
-	var buf Buffer
-	count := 10000
-	expectedSlice := make([][]byte, 0, count)
+func TestBufferWrite(t *testing.T) {
+	rand.Seed(time.Now().Unix())
 
-	// Create "count" number of slices.
-	for i := 0; i < count; i++ {
-		sz := rand.Intn(1000)
-		testBuf := make([]byte, sz)
-		rand.Read(testBuf)
+	for btype := UseCalloc; btype < UseInvalid; btype++ {
+		name := fmt.Sprintf("Using mode %s", btype)
+		t.Run(name, func(t *testing.T) {
+			var wb [128]byte
+			rand.Read(wb[:])
 
-		newSlice := buf.SliceAllocate(sz)
-		require.Equal(t, sz, copy(newSlice, testBuf))
+			cb, err := NewBufferWith(32, 4<<30, btype)
+			require.Nil(t, err)
+			defer cb.Release()
 
-		// Save testBuf for verification.
-		expectedSlice = append(expectedSlice, testBuf)
+			bb := new(bytes.Buffer)
+
+			end := 32
+			for i := 0; i < 3; i++ {
+				n, err := cb.Write(wb[:end])
+				require.NoError(t, err, "unable to write bytes to buffer")
+				require.Equal(t, n, end, "length of buffer and length written should be equal")
+
+				// append to bb also for testing.
+				bb.Write(wb[:end])
+
+				require.True(t, bytes.Equal(cb.Bytes(), bb.Bytes()), "Both bytes should match")
+				end = end * 2
+			}
+		})
 	}
+}
 
-	offsets := buf.SliceOffsets(nil)
-	require.Equal(t, len(expectedSlice), len(offsets))
-	for i, off := range offsets {
-		// All the slices returned by the buffer should be equal to what we
-		// inserted earlier.
-		require.Equal(t, expectedSlice[i], buf.Slice(off))
+func TestBufferSimpleSort(t *testing.T) {
+	buf := NewBuffer(1 << 20)
+	for i := 0; i < 25600; i++ {
+		b := buf.SliceAllocate(4)
+		binary.BigEndian.PutUint32(b, uint32(rand.Int31n(256000)))
+	}
+	buf.SortSlice(func(ls, rs []byte) bool {
+		left := binary.BigEndian.Uint32(ls)
+		right := binary.BigEndian.Uint32(rs)
+		return left < right
+	})
+	var last uint32
+	var i int
+	slice, next := []byte{}, 1
+	for next != 0 {
+		slice, next = buf.Slice(next)
+		num := binary.BigEndian.Uint32(slice)
+		if num < last {
+			fmt.Printf("num: %d idx: %d last: %d\n", num, i, last)
+		}
+		i++
+		require.GreaterOrEqual(t, num, last)
+		last = num
+		// fmt.Printf("Got number: %d\n", num)
+	}
+}
+
+func TestBufferSlice(t *testing.T) {
+	for btype := UseCalloc; btype < UseInvalid; btype++ {
+		name := fmt.Sprintf("Using mode %s", btype)
+		t.Run(name, func(t *testing.T) {
+			buf, err := NewBufferWith(0, 0, btype)
+			require.Nil(t, err)
+			defer buf.Release()
+
+			count := 10000
+			exp := make([][]byte, 0, count)
+
+			// Create "count" number of slices.
+			for i := 0; i < count; i++ {
+				sz := 1 + rand.Intn(8)
+				testBuf := make([]byte, sz)
+				rand.Read(testBuf)
+
+				newSlice := buf.SliceAllocate(sz)
+				require.Equal(t, sz, copy(newSlice, testBuf))
+
+				// Save testBuf for verification.
+				exp = append(exp, testBuf)
+			}
+
+			compare := func() {
+				next, i := 1, 0
+				for next != 0 {
+					// All the slices returned by the buffer should be equal to what we
+					// inserted earlier.
+					var slice []byte
+					slice, next = buf.Slice(next)
+					if !bytes.Equal(exp[i], slice) {
+						fmt.Printf("exp: %s got: %s\n", hex.Dump(exp[i]), hex.Dump(slice))
+						t.Fail()
+					}
+					require.Equal(t, exp[i], slice)
+					i++
+				}
+				require.Equal(t, len(exp), i)
+			}
+			compare() // same order as inserted.
+
+			t.Logf("Sorting using sort.Slice\n")
+			sort.Slice(exp, func(i, j int) bool {
+				return bytes.Compare(exp[i], exp[j]) < 0
+			})
+			t.Logf("Sorting using buf.SortSlice\n")
+			buf.SortSlice(func(a, b []byte) bool {
+				return bytes.Compare(a, b) < 0
+			})
+			t.Logf("Done sorting\n")
+			compare() // same order after sort.
+		})
+	}
+}
+
+func TestBufferSort(t *testing.T) {
+	for btype := UseCalloc; btype < UseInvalid; btype++ {
+		name := fmt.Sprintf("Using mode %s", btype)
+		t.Run(name, func(t *testing.T) {
+			buf, err := NewBufferWith(0, 0, btype)
+			require.Nil(t, err)
+			defer buf.Release()
+
+			N := 10000
+
+			for i := 0; i < N; i++ {
+				newSlice := buf.SliceAllocate(8)
+				uid := uint64(rand.Int63())
+				binary.BigEndian.PutUint64(newSlice, uid)
+			}
+
+			test := func(start, end int) {
+				start = 1 + 12*start
+				end = 1 + 12*end
+				buf.SortSliceBetween(start, end, func(ls, rs []byte) bool {
+					lhs := binary.BigEndian.Uint64(ls)
+					rhs := binary.BigEndian.Uint64(rs)
+					return lhs < rhs
+				})
+
+				slice, next := []byte{}, start
+				var last uint64
+				var count int
+				for next != 0 && next < end {
+					slice, next = buf.Slice(next)
+					uid := binary.BigEndian.Uint64(slice)
+					require.GreaterOrEqual(t, uid, last)
+					last = uid
+					count++
+				}
+				require.Equal(t, (end-start)/12, count)
+			}
+			for i := 10; i <= N; i += 10 {
+				test(i-10, i)
+			}
+			test(0, N)
+		})
 	}
 }
