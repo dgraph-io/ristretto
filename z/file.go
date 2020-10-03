8795246c0391e57b189600336ff2122f03f74315
Manish R Jain <manish@dgraph.io>
2020-10-02 20:25:18 -0700

    Add a bunch of APIs around MmapFile

--- z/file.go
+++ z/file.go
@@ -3,6 +3,7 @@ package z
 import (
 	"encoding/binary"
 	"fmt"
+	"io"
 	"os"
 	"path"
 
@@ -27,21 +28,30 @@ func OpenMmapFile(filename string, flag int, maxSz int) (*MmapFile, error) {
 	if err != nil {
 		return nil, errors.Wrapf(err, "unable to open: %s", filename)
 	}
+
 	fi, err := fd.Stat()
 	if err != nil {
 		return nil, errors.Wrapf(err, "cannot stat file: %s", filename)
 	}
+
 	fileSize := fi.Size()
-	if fileSize > int64(maxSz) {
+
+	// If file is empty, truncate it to maxSz.
+	if fileSize == 0 {
+		if err := fd.Truncate(int64(maxSz)); err != nil {
+			return nil, errors.Wrapf(err, "error while truncation")
+		}
+
+	} else if fileSize > int64(maxSz) {
 		return nil, errors.Errorf("file size %d does not match zero or max size %d",
 			fileSize, maxSz)
+	} else {
+		maxSz = int(fileSize)
 	}
-	if err := fd.Truncate(int64(maxSz)); err != nil {
-		return nil, errors.Wrapf(err, "error while truncation")
-	}
-	buf, err := Mmap(fd, true, int64(maxSz)) // Mmap up to max size.
+
+	buf, err := Mmap(fd, true, int64(maxSz)) // Mmap up to file size.
 	if err != nil {
-		return nil, errors.Wrapf(err, "while mmapping %s with size: %d", fd.Name(), maxSz)
+		return nil, errors.Wrapf(err, "while mmapping %s with size: %d", fd.Name(), fileSize)
 	}
 
 	err = nil
@@ -54,6 +64,30 @@ func OpenMmapFile(filename string, flag int, maxSz int) (*MmapFile, error) {
 		Data: buf,
 		Fd:   fd,
 	}, err
+}
+
+type mmapReader struct {
+	Data   []byte
+	offset int
+}
+
+func (mr *mmapReader) Read(buf []byte) (int, error) {
+	if mr.offset > len(mr.Data) {
+		return 0, io.EOF
+	}
+	n := copy(buf, mr.Data[mr.offset:])
+	mr.offset += n
+	if n < len(buf) {
+		return n, io.EOF
+	}
+	return n, nil
+}
+
+func (m *MmapFile) NewReader(offset int) io.Reader {
+	return &mmapReader{
+		Data:   m.Data,
+		offset: offset,
+	}
 }
 
 // Slice returns the slice at the given offset.
@@ -82,10 +116,38 @@ func (m *MmapFile) Delete() error {
 	if err := Munmap(m.Data); err != nil {
 		return fmt.Errorf("while munmap file: %s, error: %v\n", m.Fd.Name(), err)
 	}
+	m.Data = nil
 	if err := m.Fd.Truncate(0); err != nil {
 		return fmt.Errorf("while truncate file: %s, error: %v\n", m.Fd.Name(), err)
 	}
 	return os.Remove(m.Fd.Name())
+}
+
+// Truncate would truncate the mmapped file to the given size. On Linux and
+// others, we could directly just truncate the underlying file, but in Windows,
+// we can't do that. So, unmap first, then truncate, then re-map.
+func (m *MmapFile) Truncate(maxSz int64) error {
+	if err := Munmap(m.Data); err != nil {
+		return fmt.Errorf("while munmap file: %s, error: %v\n", m.Fd.Name(), err)
+	}
+	if err := m.Fd.Truncate(maxSz); err != nil {
+		return fmt.Errorf("while truncate file: %s, error: %v\n", m.Fd.Name(), err)
+	}
+	var err error
+	m.Data, err = Mmap(m.Fd, true, maxSz) // Mmap up to max size.
+	return err
+}
+
+func (m *MmapFile) Close(maxSz int64) error {
+	if err := Munmap(m.Data); err != nil {
+		return fmt.Errorf("while munmap file: %s, error: %v\n", m.Fd.Name(), err)
+	}
+	if maxSz >= 0 {
+		if err := m.Fd.Truncate(maxSz); err != nil {
+			return fmt.Errorf("while truncate file: %s, error: %v\n", m.Fd.Name(), err)
+		}
+	}
+	return m.Fd.Close()
 }
 
 func SyncDir(dir string) error {
