2729933817d48d4df20526e576a796de6535f4f7
Chewxy <chewxy@gmail.com>
2020-11-05 09:05:57 +1100

    Fixmemtest (#213)
    * Fixed memtest for easy comparison
    
    * Added README

---
+++ contrib/memtest/withjemalloc.go
@@ -0,0 +1,27 @@
+//+build jemalloc
+
+package main
+
+import (
+	"log"
+
+	"github.com/dgraph-io/ristretto/z"
+)
+
+func Calloc(size int) []byte { return z.Calloc(size) }
+func Free(bs []byte)         { z.Free(bs) }
+func NumAllocBytes() int64   { return z.NumAllocBytes() }
+
+func check() {
+	if buf := z.CallocNoRef(1); len(buf) == 0 {
+		log.Fatalf("Not using manual memory management. Compile with jemalloc.")
+	} else {
+		z.Free(buf)
+	}
+
+	z.StatsPrint()
+}
+
+func init() {
+	log.Printf("USING JEMALLOC")
+}
