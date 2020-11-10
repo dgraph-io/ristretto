f30e50eaf8dfcde5e3968f3e055fdbcf28955d7b
Ajeet D'Souza <98ajeet@gmail.com>
2020-11-10 14:07:51 +0530

    Use mremap on Linux instead of munmap+mmap (#221)

--- z/file.go
+++ z/file.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Dgraph Labs, Inc. and Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package z
 
 import (
@@ -150,24 +166,6 @@ func (m *MmapFile) Delete() error {
 		return fmt.Errorf("while truncate file: %s, error: %v\n", m.Fd.Name(), err)
 	}
 	return os.Remove(m.Fd.Name())
-}
-
-// Truncate would truncate the mmapped file to the given size. On Linux and
-// others, we could directly just truncate the underlying file, but in Windows,
-// we can't do that. So, unmap first, then truncate, then re-map.
-func (m *MmapFile) Truncate(maxSz int64) error {
-	if err := m.Sync(); err != nil {
-		return fmt.Errorf("while sync file: %s, error: %v\n", m.Fd.Name(), err)
-	}
-	if err := Munmap(m.Data); err != nil {
-		return fmt.Errorf("while munmap file: %s, error: %v\n", m.Fd.Name(), err)
-	}
-	if err := m.Fd.Truncate(maxSz); err != nil {
-		return fmt.Errorf("while truncate file: %s, error: %v\n", m.Fd.Name(), err)
-	}
-	var err error
-	m.Data, err = Mmap(m.Fd, true, maxSz) // Mmap up to max size.
-	return err
 }
 
 // Close would close the file. It would also truncate the file if maxSz >= 0.
