729b3243a2c9858cc801702169ee0c780b2bad4e
Chewxy <chewxy@gmail.com>
2020-11-20 11:49:04 +1100

    Updated ZeroOut to use a simple for loop.  (#231)
    * Updated ZeroOut to use a simple for loop. See also: https://github.com/dgraph-io/ristretto-benchmarks/tree/main/zeroout

--- z/z.go
+++ z/z.go
@@ -126,19 +126,19 @@ func (lc *Closer) SignalAndWait() {
 	lc.Wait()
 }
 
-var zeroBuf = make([]byte, 1024)
-
 // ZeroOut zeroes out all the bytes in the range [start, end).
 func ZeroOut(dst []byte, start, end int) {
+	if start < 0 || start >= len(dst) {
+		return // BAD
+	}
+	if end >= len(dst) {
+		end = len(dst)
+	}
 	if end-start <= 0 {
 		return
 	}
-	buf := dst[start:end]
-	n := copy(buf, zeroBuf)
-	if n < len(zeroBuf) {
-		return
-	}
-	for i := n; i < len(buf); i *= 2 {
-		copy(buf[i:], buf[:i])
+	b := dst[start:end]
+	for i := range b {
+		b[i] = 0x0
 	}
 }
