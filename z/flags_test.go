9b320d0a70edeefc548b69cf26d31e6faadd7395
Karl McGuire <karl@karlmcguire.com>
2021-03-08 16:55:31 -0500

    fix(Flags): panic on user errors (#256)

--- z/flags_test.go
+++ z/flags_test.go
@@ -18,13 +18,10 @@ func TestFlag(t *testing.T) {
 	// bool-key and int-key should not be overwritten. Only other-key should be set.
 	sf.MergeAndCheckDefault(def)
 
-	/* TODO
-	c := func() {
-		// Has a typo.
+	require.Panics(t, func() {
+		// typo
 		NewSuperFlag("boolo-key=true").MergeAndCheckDefault(def)
-	}
-	require.Panics(t, c)
-	*/
+	})
 
 	require.Equal(t, true, sf.GetBool("bool-key"))
 	require.Equal(t, uint64(5), sf.GetUint64("int-key"))
@@ -34,4 +31,11 @@ func TestFlag(t *testing.T) {
 	require.Equal(t, time.Minute*15, sf.GetDuration("duration-minutes"))
 	require.Equal(t, time.Hour*12, sf.GetDuration("duration-hours"))
 	require.Equal(t, time.Hour*24*30, sf.GetDuration("duration-days"))
+}
+
+func TestFlagDefault(t *testing.T) {
+	def := `one=false; two=; three=;`
+	f := NewSuperFlag(`one=true; two=4;`).MergeAndCheckDefault(def)
+	require.Equal(t, true, f.GetBool("one"))
+	require.Equal(t, int64(4), f.GetInt64("two"))
 }
