e0498e1d76f2fefffb8d3688cb60fe66d8d4afe7
Ajeet D'Souza <98ajeet@gmail.com>
2021-03-10 13:23:43 +0530

    Check for keys without values in superflags

--- z/flags_test.go
+++ z/flags_test.go
@@ -8,20 +8,18 @@ import (
 )
 
 func TestFlag(t *testing.T) {
-	opt := `bool_key=true; int-key=5; float-key=0.05; string_key=value; ;`
-	sf := NewSuperFlag(opt)
-	t.Logf("Got SuperFlag: %s\n", sf)
-
-	def := `bool_key=false; int-key=0; float-key=1.0; string-key=; other-key=5;
+	const opt = `bool_key=true; int-key=5; float-key=0.05; string_key=value; ;`
+	const def = `bool_key=false; int-key=0; float-key=1.0; string-key=; other-key=5;
 		duration-minutes=15m; duration-hours=12h; duration-days=30d;`
 
-	// bool-key and int-key should not be overwritten. Only other-key should be set.
-	sf.MergeAndCheckDefault(def)
+	_, err := NewSuperFlag("boolo-key=true").mergeAndCheckDefaultImpl(def)
+	require.Error(t, err)
+	_, err = newSuperFlagImpl("key-without-value")
+	require.Error(t, err)
 
-	require.Panics(t, func() {
-		// typo
-		NewSuperFlag("boolo-key=true").MergeAndCheckDefault(def)
-	})
+	// bool-key and int-key should not be overwritten. Only other-key should be set.
+	sf := NewSuperFlag(opt)
+	sf.MergeAndCheckDefault(def)
 
 	require.Equal(t, true, sf.GetBool("bool-key"))
 	require.Equal(t, uint64(5), sf.GetUint64("int-key"))
