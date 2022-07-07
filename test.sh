ecb695aea1c4d1f69342da00308cdac4f5082968
Joaquin Menchaca <joaquin@dgraph.io>
2022-07-07 14:14:38 -0700

    test.sh - tabs to spaces

--- test.sh
+++ test.sh
@@ -3,20 +3,20 @@ DEFAULT_VERSION="1.17"
 GOVERSION=$({ [ -f .go-version ] && cat .go-version; } || echo $DEFAULT_VERSION)
 
 starttest() {
-	set -e
-	GO111MODULE=on go test -race ./...
+  set -e
+  GO111MODULE=on go test -race ./...
 }
 
 if [ -z "${TEAMCITY_VERSION}" ]; then
-	# running locally, so start test in a container
-	# TEAMCITY_VERSION=local will avoid recursive calls, when it would be running in container
-	docker run --rm --name ristretto-test --tty --interactive \
-	  --volume `pwd`:/go/src/github.com/dgraph-io/ristretto \
-  	--workdir /go/src/github.com/dgraph-io/ristretto \
-		--env TEAMCITY_VERSION=local \
-  	golang:$GOVERSION \
-  	sh test.sh
+  # running locally, so start test in a container
+  # TEAMCITY_VERSION=local will avoid recursive calls, when it would be running in container
+  docker run --rm --name ristretto-test --tty --interactive \
+    --volume `pwd`:/go/src/github.com/dgraph-io/ristretto \
+    --workdir /go/src/github.com/dgraph-io/ristretto \
+    --env TEAMCITY_VERSION=local \
+    golang:$GOVERSION \
+    sh test.sh
 else
-	# running in teamcity, since teamcity itself run this in container, let's simply run this
-	starttest
+  # running in teamcity, since teamcity itself run this in container, let's simply run this
+  starttest
 fi
