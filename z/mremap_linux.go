d536f6f37c5c69811926318a96789240c6817fb0
mattthew <matthew.mcneely@gmail.com>
2022-10-10 17:55:58 -0400

    Add lint ignore directives

--- z/mremap_linux.go
+++ z/mremap_linux.go
@@ -28,6 +28,7 @@ import (
 
 // mremap is a Linux-specific system call to remap pages in memory. This can be used in place of munmap + mmap.
 func mremap(data []byte, size int) ([]byte, error) {
+	//nolint:lll
 	// taken from <https://github.com/torvalds/linux/blob/f8394f232b1eab649ce2df5c5f15b0e528c92091/include/uapi/linux/mman.h#L8>
 	const MREMAP_MAYMOVE = 0x1
 
